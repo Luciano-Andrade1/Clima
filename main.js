82a201c31a176d92f6f4267f586c49d6831c4cf2
Luciano-Andrade1 <duarteluciano947@gmail.com>
2025-04-04 20:14:13 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+// minha chave da API
+const key ="e0283766cee7e7e70be7e75409ae8042"
+
+//função para pegar o valor do input-cidadde
+
+function coletarValor() {
+    // buscando o valor do input no HTML e atribuindo a variavel cidade
+    let cidade = document.querySelector(".input-cidade").value
+    // imprimindo a variavel cidade
+    console.log(cidade)
+    dadosAPI(cidade)
+}
+
+//função para pegar dados da Api
+async function dadosAPI(cidade) {
+    let dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(response => response.json())
+    console.log(dados)
+    dadosTela(dados)
+}
+function dadosTela(dados){
+    document.querySelector(".cidade").innerHTML = dados.name;
+    document.querySelector(".temperatura").innerHTML = Math.floor(dados.main.temp) + "°C"
+    document.querySelector(".icone-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+    document.querySelector(".previsao").innerHTML = dados.weather[0].description
+    document.querySelector(".umidade").innerHTML = "Umidade: " + dados.main.humidity + "%"
+}
